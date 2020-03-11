972c95edf77c3dd0c87a833e56b8bb3ec47ada4c
liuyang <liuyang@coohua.com>
2020-03-11 08:53:10 +0800

    查找最近5天的破绽票

--- FindLeakStocks.py
+++ FindLeakStocks.py
@@ -1,43 +1,54 @@
-import time
-
 from StockModel import StockModel
 from basic import Stock
 
-# 获得当天的日期
-timestamp = time.time()
-today = time.strftime("%Y%m%d", time.localtime(timestamp))
 
-stocks = []
-stock_codes = []
-stocksFileContent = open('observed_stocks.data', 'r')
-for stockLineData in stocksFileContent:
-    if stockLineData.startswith('#'):
-        continue
-    stockLineData = stockLineData.strip('\n')
-    stock_detail = stockLineData.split()
-    name = stock_detail[0]
-    code = stock_detail[1]
-    stock_codes.append(code)
-    start = stock_detail[2]
-    to = stock_detail[3]
-    stock = StockModel.StockModel(name, code, int(start), int(to))
-    stocks.append(stock)
+def get_stocks():
+    stocks = []
+    stock_codes = []
+    stocks_file_content = open('observed_stocks.data', 'r')
+    for stock_line_data in stocks_file_content:
+        if stock_line_data.startswith('#'):
+            continue
+        stock_line_data = stock_line_data.strip('\n')
+        stock_detail = stock_line_data.split()
+        name = stock_detail[0]
+        code = stock_detail[1]
+        stock_codes.append(code)
+        start = stock_detail[2]
+        to = stock_detail[3]
+        stock = StockModel.StockModel(name, code, int(start), int(to))
+        stocks.append(stock)
+    return stocks, stock_codes
 
-todayStockData = Stock.get_stock_basic(','.join(stock_codes), today)
 
-# 保证请求股票的数量与返回的数量是一致的
-assert (todayStockData.shape[0] == len(stocks))
+def get_stock_data_and_calc(day):
+    stocks, stock_codes = get_stocks()
+    today_stock_data = Stock.get_stock_basic(','.join(stock_codes), day)
+    # 保证请求股票的数量与返回的数量是一致的
+    if today_stock_data.shape[0] != len(stocks):
+        print(day + ' 数据有些问题')
+        print(today_stock_data)
+        return
 
-codeStockDict = {}
+    code_stock_dict = {}
 
-for index, row in todayStockData.iterrows():
-    codeStockDict[row[0]] = row
+    for index, row in today_stock_data.iterrows():
+        code_stock_dict[row[0]] = row
 
-for stock in stocks:
-    series = codeStockDict[stock.code]
-    stock.openP = series[2] * 100
-    stock.low = series[4] * 100
-    stock.close = series[5] * 100
-    stock.calc()
-    if stock.is_leak:
-        print(stock.name)
+    today_leaks = []
+    for stock in stocks:
+        series = code_stock_dict[stock.code]
+        stock.openP = series[2] * 100
+        stock.low = series[4] * 100
+        stock.close = series[5] * 100
+        stock.calc()
+        if stock.is_leak:
+            today_leaks.append(stock.name)
+    print(day + '的破绽票有:')
+    print(today_leaks)
+
+
+if __name__ == '__main__':
+    trade_days = Stock.get_trade_cal(5)
+    for day in trade_days:
+        get_stock_data_and_calc(day)
